c298356189efd72d905c0c74318e6a13392db6db
Felix Klauke <info@felix-klauke.de>
2018-03-17 15:40:44 +0100

    Add kingdom table creation.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- core/src/main/java/de/felix_klauke/fallout/core/provider/DataSourceProvider.java
+++ core/src/main/java/de/felix_klauke/fallout/core/provider/DataSourceProvider.java
@@ -7,6 +7,9 @@ import de.felix_klauke.fallout.core.config.FalloutCoreConfig;
 import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * @author Felix Klauke <info@felix-klauke.de>
@@ -25,6 +28,29 @@ public class DataSourceProvider implements Provider<DataSource> {
         hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250");
         hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
         dataSource = new HikariDataSource(hikariConfig);
+
+        createTables();
+    }
+
+    private void createTables() {
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement("create table fallout_kingdoms (\n" +
+                    "  id          int auto_increment primary key,\n" +
+                    "  uniqueId    varchar(36)  not null,\n" +
+                    "  name        varchar(16)  not null,\n" +
+                    "  description varchar(255) null,\n" +
+                    "  constraint fallout_kingdoms_id_uindex\n" +
+                    "  unique (id),\n" +
+                    "  constraint fallout_kingdoms_uniqueId_uindex\n" +
+                    "  unique (uniqueId),\n" +
+                    "  constraint fallout_kingdoms_name_uindex\n" +
+                    "  unique (name)\n" +
+                    ") engine = InnoDB;");
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
