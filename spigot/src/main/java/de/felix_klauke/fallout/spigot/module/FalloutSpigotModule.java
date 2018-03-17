bc881bfdc152995c8b3eab8e47f09a850ada23b7
Felix Klauke <info@felix-klauke.de>
2018-03-17 15:15:52 +0100

    Properly create core config.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- spigot/src/main/java/de/felix_klauke/fallout/spigot/module/FalloutSpigotModule.java
+++ spigot/src/main/java/de/felix_klauke/fallout/spigot/module/FalloutSpigotModule.java
@@ -2,10 +2,12 @@ package de.felix_klauke.fallout.spigot.module;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
+import de.felix_klauke.fallout.core.config.FalloutCoreConfig;
 import de.felix_klauke.fallout.core.module.FalloutCoreModule;
 import de.felix_klauke.fallout.spigot.FalloutSpigotApplication;
 import de.felix_klauke.fallout.spigot.FalloutSpigotApplicationImpl;
 import de.felix_klauke.fallout.spigot.FalloutSpigotPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 
 /**
@@ -14,14 +16,18 @@ import org.bukkit.plugin.Plugin;
 public class FalloutSpigotModule extends AbstractModule {
 
     private final FalloutSpigotPlugin falloutSpigotPlugin;
+    private final FalloutCoreConfig coreConfig;
 
     public FalloutSpigotModule(FalloutSpigotPlugin falloutSpigotPlugin) {
         this.falloutSpigotPlugin = falloutSpigotPlugin;
+
+        FileConfiguration config = falloutSpigotPlugin.getConfig();
+        coreConfig = new FalloutCoreConfig(config.getString("database.url"), config.getString("database.user"), config.getString("database.password"));
     }
 
     @Override
     protected void configure() {
-        install(new FalloutCoreModule(falloutCoreConfig));
+        install(new FalloutCoreModule(coreConfig));
 
         bind(Plugin.class).annotatedWith(Names.named("falloutPlugin")).toInstance(falloutSpigotPlugin);
 
